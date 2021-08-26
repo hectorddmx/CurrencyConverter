962fc11eeb5ff4da1bd7a634ab178cc1f79ecc7c
Hector De Diego <h.dediego@distillery.com>
2021-08-26 01:03:10 -0500

    style: fix naming scheme
    https://dart.dev/guides/language/effective-dart/style

--- test/responses_test.dart
+++ test/responses_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'dart:convert';
-import 'package:currency_converter/models/ConvertResponse.dart';
+import 'package:currency_converter/models/convert_response.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
