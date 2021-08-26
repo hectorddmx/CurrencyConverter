ac80724697ca4b1669069eaa29dc13373674292e
Hector De Diego <h.dediego@distillery.com>
2021-08-25 19:50:51 -0500

    feat: source currency card

--- lib/main.dart
+++ lib/main.dart
@@ -92,14 +92,51 @@ class _MyHomePageState extends State<MyHomePage> {
               // axis because Columns are vertical (the cross axis would be
               // horizontal).
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[]
-          )
-      ),
+              children: <Widget>[
+                SourceCurrencyWidget(),
+              ])),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SourceCurrencyWidget extends StatelessWidget {
+  const SourceCurrencyWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album),
+            title: Text('Upper card'),
+            subtitle: Text('Description.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
