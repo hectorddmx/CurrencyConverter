02e1df2d2ece8b1976b201bb6ea69c5c7248449d
Hector De Diego <h.dediego@distillery.com>
2021-08-26 00:52:49 -0500

    style: formatting
    max column

--- lib/models/ConvertResponse.dart
+++ lib/models/ConvertResponse.dart
@@ -46,7 +46,8 @@ class ConvertResponse {
         "query": query?.toJson(),
         "info": info?.toJson(),
         "date":
-            "${date?.year.toString().padLeft(4, '0')}-${date?.month.toString().padLeft(2, '0')}-${date?.day.toString().padLeft(2, '0')}",
+            "${date?.year.toString().padLeft(4, '0')}-${date?.month.toString()
+                .padLeft(2, '0')}-${date?.day.toString().padLeft(2, '0')}",
         "result": result,
         // "error": error?.toJson(),
       };
